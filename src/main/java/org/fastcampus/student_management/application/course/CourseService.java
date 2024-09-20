ef4192609aaccad37af75641a5feb2a4d55f3643
seungwon-choi <bestwondev@gmail.com>
2024-09-20 11:22:41 +0900

    Feat: 일별 수업 정보 반환 기능

--- src/main/java/org/fastcampus/student_management/application/course/CourseService.java
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -2,6 +2,7 @@ package org.fastcampus.student_management.application.course;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
 import org.fastcampus.student_management.domain.Course;
@@ -10,26 +11,36 @@ import org.fastcampus.student_management.domain.Student;
 import org.fastcampus.student_management.repo.CourseRepository;
 
 public class CourseService {
-  private final CourseRepository courseRepository;
-  private final StudentService studentService;
+    private final CourseRepository courseRepository;
+    private final StudentService studentService;
 
-  public CourseService(CourseRepository courseRepository, StudentService studentService) {
-    this.courseRepository = courseRepository;
-    this.studentService = studentService;
-  }
+    public CourseService(CourseRepository courseRepository, StudentService studentService) {
+        this.courseRepository = courseRepository;
+        this.studentService = studentService;
+    }
 
-  public void registerCourse(CourseInfoDto courseInfoDto) {
-    Student student = studentService.getStudent(courseInfoDto.getStudentName());
-    Course course = new Course(student, courseInfoDto.getCourseName(), courseInfoDto.getFee(), courseInfoDto.getDayOfWeek(), courseInfoDto.getCourseTime());
-    courseRepository.save(course);
-  }
+    public void registerCourse(CourseInfoDto courseInfoDto) {
+        Student student = studentService.getStudent(courseInfoDto.getStudentName());
+        Course course = new Course(student, courseInfoDto.getCourseName(), courseInfoDto.getFee(), courseInfoDto.getDayOfWeek(), courseInfoDto.getCourseTime());
+        courseRepository.save(course);
+    }
 
-  public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
-    // TODO: 과제 구현 부분
-    return new ArrayList<>();
-  }
+    public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
 
-  public void changeFee(String studentName, int fee) {
-    // TODO: 과제 구현 부분
-  }
+        // TODO: 과제 구현 부분
+        List<Course> courseList = courseRepository.getCourseDayOfWeek(dayOfWeek);
+        List<CourseInfoDto> courseInfoDtoList = new ArrayList<>();
+
+        for (Course course : courseList) {
+            if (course.isActivateUser()) {
+                courseInfoDtoList.add(new CourseInfoDto(course));
+            }
+        }
+
+        return courseInfoDtoList;
+    }
+
+    public void changeFee(String studentName, int fee) {
+        // TODO: 과제 구현 부분
+    }
 }
