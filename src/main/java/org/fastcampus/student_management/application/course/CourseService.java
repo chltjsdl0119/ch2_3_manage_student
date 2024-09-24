ddcd1c18c6b6b1399a6e55483e748dd0b2507d42
seungwon-choi <bestwondev@gmail.com>
2024-09-24 12:24:33 +0900

    Feat: 수강료 변경 기능

--- src/main/java/org/fastcampus/student_management/application/course/CourseService.java
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -1,14 +1,14 @@
 package org.fastcampus.student_management.application.course;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
 import org.fastcampus.student_management.domain.Course;
 import org.fastcampus.student_management.domain.DayOfWeek;
 import org.fastcampus.student_management.domain.Student;
 import org.fastcampus.student_management.repo.CourseRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseService {
     private final CourseRepository courseRepository;
@@ -42,5 +42,9 @@ public class CourseService {
 
     public void changeFee(String studentName, int fee) {
         // TODO: 과제 구현 부분
+        List<Course> courseList = courseRepository.getCourseListByStudent(studentName);
+        for (Course course : courseList) {
+            course.changeFee(fee);
+        }
     }
 }
